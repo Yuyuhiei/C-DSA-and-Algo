1658caad46ca0408dd33bf47b35c0714c8cd63dc
Lauvigne Lumeda <lumedalauvigne@gmail.com>
2024-01-30 10:43:24 +0800

    added leetCode136 and primeNumber

---
+++ DSA Problems/primeNumber.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+int x;
+    int i, val1, val2, count=0;
+    printf("Please enter a number (positive only): ");
+    scanf_s("%d", &x);
+
+    val1 = ceil(sqrt(x));
+
+    for(i = 2; i <= val1; i++){
+        if(x % i == 0)
+            count = 1;
+    }
+
+    if ((count == 0) || x == 2 || x == 3)
+        printf("%d is a prime number", x);
+    else
+        printf("%d is not a prime number", x);
+}
---
+++ LeetCodes/LeetCode75/LeetCode136.c
@@ -0,0 +1,21 @@
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//
+//You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+#include <stdio.h>
+
+int singleNumber(int* nums, int numsSize) {
+    int x = 0;
+    for(int i = 0; i < numsSize; i++){
+        x = x ^ nums[i];
+    }
+    return x;
+}
+
+int main(){
+    int array[] = {2,2,7, 2, 3};
+    int size = 3;
+    int unique = singleNumber(array, size);
+   printf("%d", unique);
+}
+
